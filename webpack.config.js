803b0374cb0243627e91e2ff6143313b7867a3b8
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-05 11:54:25 -0600

    source map loader implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,51 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  mode: "development",
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: { loader: "babel-loader" },
-      },
-      {
-        test: /\.html$/,
-        use: [{ loader: "html-loader" }],
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' },
+            },
+            {
+                test: /\.html$/,
+                use: [{ loader: 'html-loader' }],
+            },
+            {
+                test: /\.(css|sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.jsx?$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
     ],
-  },
-  plugins: [
-    new HTMLPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
-  ],
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    compress: true,
-    open: true,
-  },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        compress: true,
+        open: true,
+    },
 };
