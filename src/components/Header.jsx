2bc293148036fdb1872209db69abfb927af06e04
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-05 12:05:29 -0600

    Header component finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import '../styles/Header.scss';
 
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <nav className="navbar">
+            <img src="./assets/icons/icon_menu.svg" alt="menu" className="menu" />
+            <section className="navbar-left">
+                <img src="./assets/logos/logo_yard_sale.svg" alt="logo" className="logo" />
+                <ul>
+                    <li>
+                        <a href="/">All</a>
+                    </li>
+                    <li>
+                        <a href="/">Clothes</a>
+                    </li>
+                    <li>
+                        <a href="/">Electronics</a>
+                    </li>
+                    <li>
+                        <a href="/">Furnitures</a>
+                    </li>
+                    <li>
+                        <a href="/">Toys</a>
+                    </li>
+                    <li>
+                        <a href="/">Others</a>
+                    </li>
+                </ul>
+            </section>
+            <section className="navbar-right">
+                <ul>
+                    <li className="navbar-email">geaglts@example.com</li>
+                    <li className="navbar-shopping-cart">
+                        <img src="./assets/icons/icon_shopping_cart.svg" alt="shopping cart" />
+                        <div>2</div>
+                    </li>
+                </ul>
+            </section>
+        </nav>
     );
 };
 
