a4ae9c37d27f5023e0cfa3aa4d43e106e8025dbf
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-05 15:16:22 -0600

    Craete new password container and change recovery password component to EmailSend

---
+++ src/containers/NewPassword.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '@styles/NewPassword.scss';
+
+import logoYardSale from '@logos/logo_yard_sale.svg';
+
+const NewPassword = () => {
+    return (
+        <div className="NewPassword">
+            <div className="form-container">
+                <img src={logoYardSale} alt="logo de yard sale" className="logo" />
+                <h1 className="title">Create a new Password</h1>
+                <p className="subtitle">Enter a new password for your account</p>
+                <form action="/" className="form">
+                    <label className="label" htmlFor="password">
+                        Password
+                    </label>
+                    <input id="password" type="password" placeholder="*********" className="input input-password" />
+                    <label className="label" htmlFor="confirmPassword">
+                        Re-enter password
+                    </label>
+                    <input id="confirmPassword" type="password" placeholder="*********" className="input input-password" />
+                    <input type="submit" value="Confirm" className="primary-button login-button" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default NewPassword;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,11 +6,12 @@ import NotFound from '@pages/NotFound';
 import Layout from '@containers/Layout';
 import Login from '@containers/Login';
 import CreateAccount from '@containers/CreateAccount';
-import RecoveryPassword from '@containers/RecoveryPassword';
+import EmailSend from '@containers/EmailSend';
 import MyAccount from '@containers/MyAccount';
 import MobileMenu from '@containers/MobileMenu';
 import MyOrder from '@containers/MyOrder';
 import MyOrders from '@containers/MyOrders';
+import NewPassword from '@containers/NewPassword';
 
 const App = () => {
     return (
@@ -20,10 +21,11 @@ const App = () => {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/signup" component={CreateAccount} />
-                    <Route exact path="/recovery-password" component={RecoveryPassword} />
+                    <Route exact path="/email-sended" component={EmailSend} />
                     <Route exact path="/my-account" component={MyAccount} />
                     <Route exact path="/my-order" component={MyOrder} />
                     <Route exact path="/my-orders" component={MyOrders} />
+                    <Route exact path="/new-password" component={NewPassword} />
                     <Route exact path="/test" component={MobileMenu} />
                     <Route path="*" component={NotFound} />
                 </Switch>
