82d9a422ddc77c2decd9f2d00ad782a808780281
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-05 12:12:22 -0600

    Create account container

---
+++ src/containers/CreateAccount.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/CreateAccount.scss';
+
+const CreateAccount = () => {
+    return (
+        <div className="createAccount">
+            <div className="form-container">
+                <h1 className="title">My account</h1>
+                <form action="/" className="form">
+                    <label className="label" for="name">
+                        Name
+                    </label>
+                    <input id="name" type="text" placeholder="geaglts" className="input input-name" />
+                    <label className="label" for="email">
+                        Email
+                    </label>
+                    <input id="email" type="email" placeholder="geaglts@example.com" className="input input-email" />
+                    <label className="label" for="password">
+                        Password
+                    </label>
+                    <input id="password" type="password" placeholder="*********" className="input input-password" />
+                    <input type="submit" value="Create" className="primary-button create-button" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default CreateAccount;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
+import CreateAccount from '../containers/CreateAccount';
 import RecoveryPassword from '../containers/RecoveryPassword';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={CreateAccount} />
                     <Route exact path="/recovery-password" component={RecoveryPassword} />
                     <Route path="*" component={NotFound} />
                 </Switch>
