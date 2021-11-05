3f961543a0908ee29cea61bb019b2bbeab8dfcc3
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-05 09:07:16 -0600

    routes  implementation

---
+++ src/containers/RecoveryPassword.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/RecoveryPassword.scss';
+
+const RecoveryPassword = () => {
+    return (
+        <div className="emailSend">
+            <div className="container">
+                <img src="./assets/logos/logo_yard_sale.svg" alt="logo de yard sale" className="logo" />
+                <h1 className="title">Email has been sent!</h1>
+                <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
+                <div className="email-image">
+                    <img src="./assets/icons/email.svg" alt="correo de yard sale" />
+                </div>
+                <button className="primary-button login-button">Login</button>
+                <p className="resend">
+                    Didn't receive the email? <a href="/">Resend</a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default RecoveryPassword;
