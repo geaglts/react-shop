a2887879910306e1f63037e23631c5d5f28f5e53
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-07 19:32:42 -0600

    Remove products from shopping cart

--- src/components/ShoppingCartProduct.jsx
+++ src/components/ShoppingCartProduct.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '@styles/ShoppingCartProduct.scss';
 import ShoppingCartOrder from '@components/ShoppingCartOrder';
 
 import iconClose from '@icons/icon_close.png';
 
+import AppContext from '@context/AppContext';
+
 const ShoppingCartProduct = ({ product }) => {
+    const { removeFromCart } = useContext(AppContext);
+
+    const handleRemove = (product) => {
+        removeFromCart(product);
+    };
+
     return (
         <div className="ShoppingCartProduct">
             <ShoppingCartOrder product={product} />
-            <button className="delete-product-button">
+            <button className="delete-product-button" onClick={() => handleRemove(product)}>
                 <img src={iconClose} alt="delete product" />
             </button>
         </div>
